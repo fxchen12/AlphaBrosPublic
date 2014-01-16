6560b7cf9db850020e5618cce5f01c332cf9e988
Keanu <Keanu@mit.edu>
2014-01-16 15:32:47 -0500

    Allows entry of user name into database.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,11 +38,11 @@ class UsersController < ApplicationController
     private
 
         def user_params
-            params.require(:user).permit(:email, :password, :password_confirmation)
+            params.require(:user).permit(:name, :email, :password, :password_confirmation)
         end
 
         def update_params
-            params.require(:user).permit(:password, :password_confirmation)
+            params.require(:user).permit(:name, :password, :password_confirmation)
         end
 
 end
