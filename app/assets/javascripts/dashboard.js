d51a2f9ca569622eb8788c9eee9cd1b4d601fb40
Keanu <Keanu@mit.edu>
2014-01-17 02:07:52 -0500

    dashboard bugfix: clicking chevron now works properly (only displays form for clicked workout).

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,8 +3,8 @@ $(function() {
     if ($(".start_image").size) {
 
         $(".start_image").click(function() {
-            var id = $(self).attr('id');
-            $(self).parent().parent().parent().find(".workout_record#"+id).toggle();
+            var id = $(this).attr('id');
+            $(this).parent().parent().parent().find(".workout_record#"+id).toggle();
         });
     }
 });
