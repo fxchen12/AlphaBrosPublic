c358ff61f6b1bf48a72cb309a3903d0fb76b24df
Keanu <Keanu@mit.edu>
2014-01-17 00:12:30 -0500

    fixed navbar/settings, trying to fix dashboard display form bug

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,7 +3,8 @@ $(function() {
     if ($(".start_image").size) {
 
         $(".start_image").click(function() {
-            $(".start_image").parent().parent().parent().find(".workout_record").toggle();
+            var id = $(self).attr('id');
+            $(self).parent().parent().parent().find(".workout_record#"+id).toggle();
         });
     }
 });
