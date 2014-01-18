dbc1234e838e61070ddf8012fb72c9f5dc742e39
Francis Chen <fxchen12@mit.edu>
2014-01-18 18:50:41 -0500

    Still working on goals.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,5 +2,16 @@ class GoalsController < ApplicationController
 
     def index
       @workouts=Workout.all
+      @goal=Goal.new
     end
+
+    def create
+      @goal = Goal.new(goal_params)
+    end
+
+    private
+
+      def goal_params
+        params.require(:goal).permit(:number, :metric, :time_range)
+      end
 end
