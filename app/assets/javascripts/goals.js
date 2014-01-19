ce090b62fade3aea22944e3f26bbe39c58e3c8da
Francis Chen <fxchen12@mit.edu>
2014-01-18 19:05:05 -0500

    More work on goals.

---
+++ app/assets/javascripts/goals.js
@@ -0,0 +1,20 @@
+$(function()) {
+    if ($("goal_button").size) {
+        if(".goal_button").click(function(){
+            var id = $(this).attr('id');
+            // $("body").find is necessary here. Do not change.
+            var form = $("body").find(".workout_goals#"+id);
+            var cover = $(".dash_cover");
+            form.show();
+            cover.show();
+            cover.click(function() {
+                cover.hide();
+                form.hide();
+            });
+            $(".xbutton").click(function() {
+                cover.hide();
+                form.hide();
+            });
+        });
+    }
+}
