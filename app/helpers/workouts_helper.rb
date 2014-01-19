280fb48fc436a0857fa87eef2e6d7e3d9f534c7d
Keanu <Keanu@mit.edu>
2014-01-18 19:04:35 -0500

    improving totals helper method

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,2 +1,24 @@
 module WorkoutsHelper
+
+    # Returns hash workout record totals per workout, sorted by date range
+    def record_totals
+        workout_hash = Hash.new
+        current_user.workouts.each do |w|
+            # Today, This Week, This Month, This Year, Total [x2 for time & distance]
+            inner_array = [
+            w.workout_records.inject(0){ |sum,r| sum += r.duration unless r.created_at.to_date != Date.today },
+            w.workout_records.inject(0){ |sum,r| sum += r.duration unless !((Date.beginning_of_week(start_day = :sunday)..Date.today) === r.created_at.to_date) },
+            w.workout_records.inject(0){ |sum,r| sum += r.duration unless !((Date.beginning_of_month..Date.today) === r.created_at.to_date) },
+            w.workout_records.inject(0){ |sum,r| sum += r.duration unless !((Date.beginning_of_year..Date.today) === r.created_at.to_date) },
+            w.workout_records.inject(0){ |sum,r| sum += r.duration,
+            w.workout_records.inject(0){ |sum,r| sum += r.distance unless r.created_at.to_date != Date.today },
+            w.workout_records.inject(0){ |sum,r| sum += r.distance unless !((Date.beginning_of_week(start_day = :sunday)..Date.today) === r.created_at.to_date) },
+            w.workout_records.inject(0){ |sum,r| sum += r.distance unless !((Date.beginning_of_month..Date.today) === r.created_at.to_date) },
+            w.workout_records.inject(0){ |sum,r| sum += r.distance unless !((Date.beginning_of_year..Date.today) === r.created_at.to_date) },
+            w.workout_records.inject(0){ |sum,r| sum += r.distance }
+            ]
+            workout_hash[w] = inner_array
+        end
+        return workout_hash
+    end
 end
--- app/models/workout_record.rb
+++ app/models/workout_record.rb
@@ -10,19 +10,5 @@ class WorkoutRecord < ActiveRecord::Base
     validates_numericality_of :distance, :only_integer => true,
         :message => "Distance must be entered as a whole number"
 
-    # Returns hash workout record totals per workout, sorted by date range
-    def totals
-        workout_hash = Hash.new
-        current_user.workouts.each do |w|
-            # Today, This Week, This Month, This Year, Total
-            inner_array = [
-            w.workout_records.inject(0){ |sum,r| sum += r.duration unless r.created_at.to_date != Date.today },
-            w.workout_records.inject(0){ |sum,r| sum += r.duration unless !((Date.beginning_of_week(start_day = :sunday)..Date.today) === r.created_at.to_date) },
-            w.workout_records.inject(0){ |sum,r| sum += r.duration unless !((Date.beginning_of_month..Date.today) === r.created_at.to_date) },
-            w.workout_records.inject(0){ |sum,r| sum += r.duration unless !((Date.beginning_of_year..Date.today) === r.created_at.to_date) },
-            w.workout_records.inject(0){ |sum,r| sum += r.duration }
-            ]
-            workout_hash[w] = inner_array
-        end
-        return workout_hash
+    include WorkoutsHelper
 end
