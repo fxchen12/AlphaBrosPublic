5254900adfaf55a2f2a6790b11796e33978f7cb7
afathman <afathman@mit.edu>
2014-01-21 15:00:37 -0500

    Just submitting my changes to the graph.

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -22,22 +22,66 @@ $(function() {
 
     $('#bar_graph').highcharts({
         chart: {
-            type: 'bar'
+            backgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgba(250, 250, 250, .50)'],
+                    [1, 'rgba(250, 250, 250, .20)']
+                ]
+            },
+            type: 'bar',
+
+            events: {
+                load: applyGraphGradient
+            }
         },
+        legend: {
+            enabled: false
+        },
+        
         title: {
             text: 'Goal Progress'
         },
         xAxis: {
-            categories: ['This Exercise']
+            categories: ['This Exercise','Another Excercise', 'Yet another one']
         },
         yAxis: {
             title: {
                 text: '% Complete'
-            }
+            },
+            minRange: 100,
+            min: 0
+
         },
         series: [{
-            name: 'Goal',
-            data: [50]
+            name: 'Percent Complete',
+            data: [50,40,100]
         }]
     });
 });
+
+
+function applyGraphGradient() {
+    
+    // Options
+    var threshold = 100,
+        colorAbove = '#4572EE',
+        colorBelow = '#EE4643';
+        
+    // internal
+    var someSeries = this.series[0];
+    
+    $.each(someSeries.points, function(){
+        if (this.y < threshold)
+        {
+           this.graphic.attr('fill', colorBelow);
+        }
+        else
+        {
+            this.graphic.attr('fill', colorAbove );
+        }
+    });
+    
+    delete someSeries.pointAttr.hover.fill;
+    delete someSeries.pointAttr[''].fill; 
+}
