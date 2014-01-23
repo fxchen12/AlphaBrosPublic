df28b603b8a002ea56830b17e88daecb6be31092
Francis Chen <fxchen12@mit.edu>
2014-01-23 16:14:02 -0500

    Changes for website.

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -26,19 +26,5 @@ $(function() {
             $(this).parent().parent().parent().parent().slideUp();
         });
 
-        $(".delete_image").click(function() {
-            var id = $(this).attr('id');
-            var del = $("body").find(".delete_confirmation#"+id);
-            del.slideDown();
-            $(".dash_cover").fadeIn();
-        });
-
-        $(".add_image").click(function() {
-            var add = $("body").find(".add_input");
-            add.slideDown();
-            $(".dash_cover").fadeIn();
-        });
-
-
     }
 });
--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -4,5 +4,18 @@ $(function() {
         $('#dropdown_form select').change(function(){
             $('#dropdown_form').submit();
         });
+
+        $(".delete_image").click(function() {
+            var id = $(this).attr('id');
+            var del = $("body").find(".delete_confirmation#"+id);
+            del.slideDown();
+            $(".dash_cover").fadeIn();
+        });
+
+        $(".add_image").click(function() {
+            var add = $("body").find(".add_input");
+            add.slideDown();
+            $(".dash_cover").fadeIn();
+        });
     }
 });
